85fe3bd26a651980c98f4cd980ce914242186fb5
ALHaithem <56702742+AL-Haithem@users.noreply.github.com>
2026-07-19 13:09:54 +0100

    Adding Not Found Routes Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
+
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
@@ -9,24 +11,29 @@ import Contact from './components/Contact'
 import Footer from './components/Footer'
 import CursorGlow from './components/ui/CursorGlow'
 import AuthModal from './components/AuthModal'
+import NotFound from './NotFound.jsx'
 
 export default function App() {
   const [authOpen, setAuthOpen] = useState(false)
 
   return (
-    <>
-      <CursorGlow />
-      <Navbar />
-      <main id="home">
-        <Hero />
-        <Works onOpenAuth={() => setAuthOpen(true)} />
-        <Skills />
-        <About />
-        <Journey />
-        <Contact />
-      </main>
-      <Footer />
-      <AuthModal open={authOpen} onClose={() => setAuthOpen(false)} />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <CursorGlow />
+        <Navbar />
+        <main id="home">
+          <Hero />
+          <Works onOpenAuth={() => setAuthOpen(true)} />
+          <Skills />
+          <About />
+          <Journey />
+          <Contact />
+        </main>
+        <Footer />
+        <AuthModal open={authOpen} onClose={() => setAuthOpen(false)} />
+
+        <Route path="*" element={<NotFound />} />  
+      </Routes>
+    </BrowserRouter>
   )
 }
